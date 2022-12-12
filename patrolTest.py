fda07c56587e703caa2ba170911b73debc6640af
Faith Johnson <fmjohnson97@aol.com>
2022-12-12 14:58:09 -0500

    circle test

--- patrolTest.py
+++ patrolTest.py
@@ -38,5 +38,10 @@ limo.EnableCommand()
 # steering_angle:(float) ackerman
 
 #go straight example
-limo.SetMotionCommand(linear_vel=0.1,angular_vel=-0.01)
-time.sleep(5)
+# limo.SetMotionCommand(linear_vel=0.1,angular_vel=-0.01)
+# time.sleep(5)
+
+#circle example
+for i in range(50):
+    limo.SetMotionCommand(linear_vel=0.1,angular_vel=-0.01, steering_angle=30)
+    time.sleep(0.1)
