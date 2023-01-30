0fb0746ed6f535f1ae5d799510f73b9084ced600
Faith Johnson <fmjohnson97@aol.com>
2023-01-30 12:59:08 -0500

    changing test to go straight

--- patrolTest.py
+++ patrolTest.py
@@ -38,8 +38,8 @@ limo.EnableCommand()
 # steering_angle:(float) ackerman
 
 #go straight example
-# limo.SetMotionCommand(linear_vel=0.1,angular_vel=-0.01)
-# time.sleep(5)
+limo.SetMotionCommand(linear_vel=0.1,angular_vel=-0.01)
+time.sleep(5)
 
 # # left circle example
 # for i in range(50):
