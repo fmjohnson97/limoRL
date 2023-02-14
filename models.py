d1c9aec10817225118305a86b1f26266b012ef07
Faith Johnson <fmjohnson97@aol.com>
2023-02-14 12:31:12 -0500

    fixing batch norm sizing

--- models.py
+++ models.py
@@ -406,7 +406,7 @@ class Encoder(nn.Module):
             BatchNorm2d(16),
             ReLU(True),
             Conv2d(16, 32, 3, stride=2, padding=0),
-            BatchNorm2d(16),
+            BatchNorm2d(32),
             ReLU(True),
             Conv2d(32, 64, 3, stride=2, padding=0),
             ReLU(True)
@@ -445,7 +445,7 @@ class Decoder(nn.Module):
 
         self.decoder_conv = Sequential(
             ConvTranspose2d(64, 32, 3, stride=2, output_padding=1),
-            BatchNorm2d(16),
+            BatchNorm2d(32),
             ReLU(True),
             ConvTranspose2d(32, 16, 3, stride=2, output_padding=1),
             BatchNorm2d(16),
