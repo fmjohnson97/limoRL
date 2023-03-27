160f8af4086415e861a49b44cecb946d9ec72195
Faith Johnson <fmjohnson97@aol.com>
2023-03-27 14:20:05 -0400

    trying dueling dqn instead of SAC

---
+++ dqn_models.py
@@ -0,0 +1,149 @@
+# dueling dqn, qnetwork, and memory from here: https://github.com/gouxiangchen/dueling-DQN-pytorch/blob/master/dueling_dqn.py
+
+import torch
+import random
+import torch.nn as nn
+import numpy as np
+
+from collections import deque
+
+class QNetwork(nn.Module):
+    def __init__(self,feat_dim=4):
+        super(QNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(feat_dim, 64)
+        self.relu = nn.ReLU()
+        self.fc_value = nn.Linear(64, 256)
+        self.fc_adv = nn.Linear(64, 256)
+
+        self.value = nn.Linear(256, 1)
+        self.adv = nn.Linear(256, 3)
+
+    def forward(self, state):
+        # breakpoint()
+        state = state.flatten(1)
+        y = self.relu(self.fc1(state))
+        value = self.relu(self.fc_value(y))
+        adv = self.relu(self.fc_adv(y))
+
+        value = self.value(value)
+        adv = self.adv(adv)
+
+        advAverage = torch.mean(adv, dim=1, keepdim=True)
+        Q = value + adv - advAverage
+
+        return Q
+
+    def select_action(self, state):
+        with torch.no_grad():
+            Q = self.forward(state)
+            action_index = torch.argmax(Q, dim=1)
+        return action_index.item()
+
+
+class Memory(object):
+    def __init__(self, memory_size: int) -> None:
+        self.memory_size = memory_size
+        self.buffer = deque(maxlen=self.memory_size)
+
+    def add(self, experience) -> None:
+        self.buffer.append(experience)
+
+    def size(self):
+        return len(self.buffer)
+
+    def sample(self, batch_size: int, continuous: bool = True):
+        if batch_size > len(self.buffer):
+            batch_size = len(self.buffer)
+        if continuous:
+            rand = random.randint(0, len(self.buffer) - batch_size)
+            return [self.buffer[i] for i in range(rand, rand + batch_size)]
+        else:
+            indexes = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
+            return [self.buffer[i] for i in indexes]
+
+    def clear(self):
+        self.buffer.clear()
+
+class DuelingDQN(nn.Module):
+    def __init__(self, args=None, device=None, img_backbone=None):
+        super(DuelingDQN,self).__init__()
+        if device is None:
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = device
+        self.img_backbone = img_backbone
+        if args is None:
+            self.gamma = 0.99
+            self.explore = 20000
+            self.init_epsilon = 0.1
+            self.final_epsilon = 0.0001
+            self.update_steps = 4
+            self.lr = 1e-4
+        else:
+            self.gamma = args.gamma
+            self.explore = args.explore
+            self.init_epsilon = args.init_epsilon
+            self.final_epsilon = args.final_epsilon
+            self.update_steps = args.target_update_freq
+            self.lr = args.lr
+
+        self.epsilon = self.init_epsilon
+
+        self.onlineQNetwork = QNetwork(args.feat_dim).to(device)
+        self.targetQNetwork = QNetwork(args.feat_dim).to(device)
+        self.targetQNetwork.load_state_dict(self.onlineQNetwork.state_dict())
+
+        self.optimizer = torch.optim.Adam(self.onlineQNetwork.parameters(), lr=self.lr)
+        self.loss_func = nn.MSELoss()
+
+    def forward(self, x, goal):
+        x = self.img_backbone.extractFeatures(x)
+        goal = self.img_backbone.extractFeatures(goal)
+        return self.onlineQNetwork(torch.cat((x,goal), axis=-1))
+
+    def update(self, batch, step):
+        # breakpoint()
+        state, action, reward, goal, next_state, goal_new, done = batch
+        # state = torch.FloatTensor(state).to(self.device)
+        goal, goal_vec = np.stack(goal[:, 0]), np.stack(goal[:, 1])
+        # goal = torch.FloatTensor(goal_imgs).to(self.device)
+        goal_new, goal_vec = np.stack(goal_new[:, 0]), np.stack(goal_new[:, 1])
+        # goal_new = torch.FloatTensor(goal_new_imgs).to(self.device)
+        # next_state = torch.FloatTensor(next_state).to(self.device)
+        action = torch.FloatTensor(action).unsqueeze(1).to(self.device)
+        reward = torch.FloatTensor(reward).unsqueeze(1).to(self.device)
+        done = torch.FloatTensor(done).unsqueeze(1).to(self.device)
+
+        if self.img_backbone is not None:
+            state_feats = self.img_backbone.extractFeatures(state)
+            goal_feats = self.img_backbone.extractFeatures(goal)
+            next_state_feats = self.img_backbone.extractFeatures(next_state)
+            goal_new_feats = self.img_backbone.extractFeatures(goal_new)
+
+        with torch.no_grad():
+            onlineQ_next = self.onlineQNetwork(torch.cat((next_state_feats,goal_new_feats), axis=-1))
+            targetQ_next = self.targetQNetwork(torch.cat((next_state_feats,goal_new_feats), axis=-1))
+            online_max_action = torch.argmax(onlineQ_next, dim=1, keepdim=True)
+            y = reward + (1 - done) * self.gamma * targetQ_next.gather(1, online_max_action.long())
+
+        # breakpoint()
+        loss = self.loss_func(self.onlineQNetwork(torch.cat((state_feats,goal_feats), axis=-1)).gather(1, action.long()), y)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if self.epsilon > self.final_epsilon:
+            self.epsilon -= (self.init_epsilon - self.final_epsilon) / self.explore
+
+        if step % self.update_steps == 0:
+            self.targetQNetwork.load_state_dict(self.onlineQNetwork.state_dict())
+
+        return loss.item()
+
+    def select_action(self, x, goal):
+        x = self.img_backbone.extractFeatures(x)
+        goal = self.img_backbone.extractFeatures(goal)
+        return self.onlineQNetwork.select_action(torch.cat((x,goal), axis=-1))
+
+    def save_models(self, save_name, goalNode, goalDirection):
+        torch.save(self.onlineQNetwork.state_dict(), save_name+str(goalNode)+'_'+str(goalDirection)+'.pt')
---
+++ dueldqnDiscOnGraph.py
@@ -0,0 +1,296 @@
+import random
+import torch
+import math
+import argparse
+import numpy as np
+import time
+
+import matplotlib.pyplot as plt
+from torchvision.models import ResNet18_Weights
+
+from dqn_models import Memory, DuelingDQN
+from models import GoalReplayBuffer, ResnetBackbone
+from graph import GraphTraverser, Graph
+
+def getArgs():
+    parser=argparse.ArgumentParser()
+
+    #file parameters
+    parser.add_argument('--config_file', type=str, default='labGraphConfig5.json', help='path to the graph config file')
+
+    # training hyperparameters
+    parser.add_argument('--batch_size', type=int, default=32, help='number of samples used to update the networks at once')
+    parser.add_argument('--epochs', type=int, default=5000, help='number of epochs for training')
+    parser.add_argument('--steps_per_epoch', type=int, default = 20, help='max number of steps for each epoch')
+    parser.add_argument('--use_policy_step', type=int, default=200, help='number of steps before using the learned policy')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate for training')
+    parser.add_argument('--save_name', default='ddqnDiscOnGraph_', help='prefix name for saving the SAC networks')
+    parser.add_argument('--target_update_freq', type=int, default=4, help='max number of samples in the replay buffer')
+    parser.add_argument('--dist_reward', action='store_true', help='use the distance reward instead')
+    parser.add_argument('--test', action='store_true', help='skip training and just test')
+    parser.add_argument('--test_freq', type=int, default=10, help='number of epochs between testing')
+    parser.add_argument('--init_epsilon', type=float, default=0.3, help='initial epsilon value')
+    parser.add_argument('--final_epsilon', type=float, default=0.0001, help='final epsilon value')
+    parser.add_argument('--explore', type=int, default=20000, help='number of epochs for training')
+    parser.add_argument('--feat_dim', type=int, default=512 * 7 * 7*2, help='number of epochs for training')
+
+
+
+
+
+    # buffer hyperparameters
+    parser.add_argument('--buffer_limit', type=int, default=50000, help='max number of samples in the replay buffer')
+    parser.add_argument('--buffer_init_steps', type=int, default=4000, help='number of random actions to take before train loop')
+    parser.add_argument('--max_reward', type=float, default=1, help='max reward achievable by agent when reaches goal')
+
+    # sac hyperparameters
+    parser.add_argument('--gamma', type=float, default=0.99, help='discount factor for SAC RL')
+
+    # resnet/backbone args
+    parser.add_argument('--return_node', type=str, default='layer4', choices=['layer1','layer2','layer3','layer4'], help='resnet layer to return features from')
+
+    return parser.parse_args()
+
+
+def train(args, device):
+    # initialize the environment and get the first observation
+    env = GraphTraverser(Graph(config_path=args.config_file), distance_reward=args.dist_reward)
+    env.randomInit()
+    print('Goal is', env.goalNode, env.goalDirection)
+    obs = env.getImg()
+
+    # initialize and populate the replay buffer
+    replay_buffer = GoalReplayBuffer(args)
+    step = 0
+    while step < args.buffer_init_steps:
+        # breakpoint()
+        action = random.choice(range(env.action_space))
+        # goal info is currently (start node,  star dir, goal node, goal dir)
+        # getting the goal images to save instead of the goal info (since that's really for debug purposes tbh)
+        goal_img = env.getGoalImg()
+        goal_vec = None #env.getGoalVector()
+        obs_new, reward, goal_info, done = env.step(action)
+        # transforming action to store it as a vector
+        # action_ind = action
+        # action = np.zeros(env.action_space)
+        # action[action_ind] = 1
+        goal_img_new = env.getGoalImg()
+        goal_vec_new = None#env.getGoalVector()
+        # sample of the shape (s, a, r, g, s', g', done)
+        # if action_ind == 0:
+        #     if (obs == obs_new).all():
+        #         rand_int = random.random()
+        #         # if rand_int > .8:
+        #         #     step += 4
+        #         #     for i in range(4):
+        #         #         replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        #     else:
+        #         step += 2
+        #         for i in range(2):
+        #             replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        # else:
+        step += 1
+        replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        # implementing hindsight experience replay
+        # replay_buffer.addHERSample([obs, action, reward, goal_img, obs_new, done], args.max_reward)
+        if done or reward >= 1 or (step>0 and step % args.steps_per_epoch==0):
+            env.randomInit()
+            obs = env.getImg()
+            # print('Goal is', env.goalNode, env.goalDirection)
+        else:
+            obs = obs_new
+
+    # breakpoint()
+    # initialize the model
+    img_backbone = ResnetBackbone(args, device)
+    # transforms = ResNet18_Weights.DEFAULT.transforms()
+    # img_backbone = Encoder32(args.hidden_dim, transforms, device)
+    # img_backbone.load_state_dict(torch.load('allNodePhotoAE_'+str(args.hidden_dim)+'hid_encoder.pt', map_location=torch.device('cpu')))
+    # img_backbone = img_backbone.to(device)
+    # img_backbone.eval()
+    model = DuelingDQN(args, device, img_backbone)
+    model.train()
+
+    # start training
+    ep_count = 0
+    ep_reward = 0
+    ep_len = 0
+    total_qloss = 0
+    total_policyloss = 0
+    all_total_rewards = []
+    env.randomInit()
+    obs = env.getImg()
+    for step in range(args.epochs*args.steps_per_epoch):
+        # initial random action or use the learned policy
+        rand_num = random.random()
+        # last part is epsilon greedy
+        if step < args.use_policy_step or rand_num < math.exp(-1. * step / 800):
+            action = random.choice(range(env.action_space))
+            # goal info is currently (start node,  star dir, goal node, goal dir)
+            # getting the goal images to save instead of the goal info (since that's really for debug purposes tbh)
+            goal_img = env.getGoalImg()
+            goal_vec = None#env.getGoalVector()
+        else:
+            goal_img = env.getGoalImg()
+            goal_vec = None#env.getGoalVector()
+            breakpoint()
+            action = model.select_action(np.stack([obs]), np.stack([goal_img]))#, np.stack([goal_vec]))
+
+        # take action in the environment and save to replay/update trackers
+        # if step > args.use_policy_step:
+        #     print('pre state+goal', (env.current_node, env.current_direction), (env.goalNode, env.goalDirection))
+        obs_new, reward, goal_info, done = env.step(action)
+        # storing the action as a vector
+        # action_ind = action
+        # action = np.zeros(env.action_space)
+        # action[action_ind] = 1
+        goal_img_new = env.getGoalImg()
+        goal_vec_new = None#env.getGoalVector()
+        # sample of the shape (s, a, r, g, s', g', done)
+        # if action_ind == 0:
+        #     if (obs == obs_new).all():
+        #         rand_int = random.random()
+        #         # if rand_int > .9:
+        #         #     replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        #     else:
+        #         for i in range(1):
+        #             replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        # else:
+        replay_buffer.addSample([obs, action, reward, (goal_img, goal_vec), obs_new, (goal_img_new, goal_vec_new), done])
+        # implementing hindsight experience replay
+        # replay_buffer.addHERSample([obs, action, reward, goal_img, obs_new, done], args.max_reward)
+        # if step>args.use_policy_step:
+        #     print('action+reward+state+goal', action, reward, (env.current_node, env.current_direction), (env.goalNode, env.goalDirection))
+        ep_reward+=reward
+        ep_len+=1
+
+        # if done or goal reached, reset the environment and trackers, save model
+        # else switch over the obs
+        if done or ep_len>=args.steps_per_epoch:
+            env.randomInit()
+            obs = env.getImg()
+            # print('Goal is', env.goalNode, env.goalDirection)
+            print('Epoch',ep_count,'completed in',ep_len,'steps with reward =',ep_reward)
+            print('\t Total Q Loss:',total_qloss,' Avg Q Loss:',total_qloss/ep_len)
+            print('\t Total Policy Loss:',total_policyloss,' Avg Policy Loss:',total_policyloss/ep_len)
+            print()
+            all_total_rewards.append(ep_reward)
+            if len(all_total_rewards)>5 and step>args.use_policy_step and np.mean(all_total_rewards[-5:]) > -11/10:
+                print('Step:',step)
+                temp = [a[1].argmax(-1) for a in replay_buffer.buffer]
+                print(len([a for a in temp if a == 0]) / len(temp), 'is the forward percentage')
+                print(len([a for a in temp if a == 1]) / len(temp), 'is the left percentage')
+                count = 0
+                temp=0
+                while count<5 and temp>-2:
+                    temp = test(args, device, model, env.goalNode, env.goalDirection)
+                    count+=1
+                if count>=5:
+                    print('Training Done!')
+                    exit(0)
+
+            ep_len=0
+            ep_reward=0
+            total_qloss = 0
+            total_policyloss = 0
+            ep_count+=1
+            # TODO: should we relabel the buffer?
+            model.save_models(args.save_name, env.goalNode, env.goalDirection)
+        else:
+            obs = obs_new
+
+        # update model: sample the buffer, do the q and policy updates, update trackers
+        sample = replay_buffer.sample()  # (s, a, r, g, s', g', done)
+        qloss= model.update(sample, step)
+        total_qloss += qloss
+        total_policyloss += 0
+
+        if step>0 and step % args.steps_per_epoch == 0 and (step // args.steps_per_epoch) % args.test_freq == 0:
+            print('Step:', step)
+            temp = [a[1].argmax(-1) for a in replay_buffer.buffer]
+            print(len([a for a in temp if a == 0]) / len(temp), 'is the forward percentage')
+            print(len([a for a in temp if a == 1]) / len(temp), 'is the left percentage')
+            test_reward = test(args, device, model, env.goalNode, env.goalDirection)
+
+    return model
+
+def test(args, device, model=None, goalNode=None, goalDirection=None):
+    print('testing!')
+    env = GraphTraverser(Graph(config_path=args.config_file), distance_reward=args.dist_reward)
+    if goalNode is not None:
+        env.goalNode = goalNode
+    if goalDirection is not None:
+        env.goalDirection = goalDirection
+    if model is None:
+        img_backbone = ResnetBackbone(args, device)
+        model = DuelingDQN(args, device, img_backbone)
+        model.load_states(args)
+
+    env.randomInit()
+    obs = env.getImg()
+    done = False
+    step = 0
+    total_reward = 0
+    actions = []
+    locations = []
+    start = [env.current_node, env.current_direction]
+    goal = [env.goalNode, env.goalDirection]
+
+    goal_img = env.getImg()
+    while not done and step < args.steps_per_epoch:
+        action = model.select_action(np.stack([obs]), np.stack([goal_img]))
+        obs_new, reward, goal_info, done = env.step(action)
+
+        actions.append(action)
+        locations.append([env.current_node, env.current_direction])
+        total_reward+=reward
+        step+=1
+
+        obs = obs_new
+
+    print('Start: Node', start[0], ',', start[1], 'degrees')
+    print('Goal: Node', goal[0], ',', goal[1], 'degrees')
+    print('Total Reward',total_reward)
+    # print('Actions: 0=straight, 1=backward, 2=left, 3=right')
+    # the actions are bad bc don't actually take action so how know how to update the angles
+    optimal_solution = env.graph.findPath(start_node=start[0], end_node=goal[0], start_direction=start[1], end_direction=goal[1], base_angle=env.base_turn_angle)
+    print('Optimal Solution:')
+    print('path', optimal_solution[0])
+    print('actions', optimal_solution[1])
+    human_actions = []
+    action_key = {0:'forward', 1:'left', 2:'right'}#3:'backward'
+    for act in actions:
+        human_actions.append(action_key[int(act)])
+    print()
+    print('Agent Actions:', list(zip(human_actions, ['to [node,dir]='] * len(locations), locations)))
+    print()
+    # action_diffs = []
+    # i=0
+    # for act in optimal_solution[1]:
+    #     if i<len(human_actions):
+    #         if human_actions[i] not in act:
+    #             while i<len(human_actions) and human_actions[i] not in act:
+    #                 action_diffs.append(human_actions[i])
+    #                 i+=1
+    #         # elif action_diffs[-1]!='same':
+    #         else:
+    #             action_diffs.append('same')
+    # for act in human_actions[i:]:
+    #     action_diffs.append(act)
+    #     print()
+    # print('Extraneous Actions:', action_diffs)
+    time.sleep(5)
+    return total_reward
+
+
+if __name__ == '__main__':
+    torch.manual_seed(525)
+    np.random.seed(525)
+
+    args = getArgs()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    if not args.test:
+        model = train(args, device)
+    test(args, device)#, model)
+
